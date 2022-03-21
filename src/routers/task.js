f1a262fb48030498c526aa74aa13d14c2fd08de6
adamsyy <adamoommenjacob@gmail.com>
2022-03-21 22:23:16 +0530

    sorting amd limits done

--- src/routers/task.js
+++ src/routers/task.js
@@ -16,6 +16,11 @@ router.get('/tasks', auth, async (req, res) => {
     if (req.query.completed) {
         match.completed = req.query.completed == 'true'
     }
+if(req.query.sortBy){
+    const parts=req.query.sortBy.split(':')
+    sort[parts[0]]=parts[1]=='asc'?1:-1
+}
+
     try {
 
         await req.user.populate({
